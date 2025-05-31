d5fc4d6fdd3498f53a13a8b3fb3f095d6cccec5e
erinpaul2002 <erinpaul121102@gmail.com>
2025-05-31 10:19:48 +0530

    added activate backend request for inactive backend

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
-
-
+'use client';
+import React, { useEffect } from 'react';
 import CodeIDE from '../components/CodeIDE';
 
 export default function Home() {
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/health`)
+      .then(() => {})
+      .catch(() => {});
+  }, []);
+
   return (
     <CodeIDE />
   );
